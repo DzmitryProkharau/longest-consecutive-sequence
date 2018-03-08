25cd127a0f7cae5608ddb2e2a1603dd82746381e
DzmitryProkharau <33517308+DzmitryProkharau@users.noreply.github.com>
2018-03-08 15:07:45 +0300

    step 1 1

--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
 module.exports = function longestConsecutiveLength(array) {
-    var sequence = 2;
-    var sum = 1;
-
-    if (array.length === 0) {
+	if (array.length == 0) {
         return 0;
-    }else if (array.length === 1) {
-        return 1;        
-    }else {
-        array.sort(function (a, b) {
-            return a - b;
-        });
-
-        for (var i = 0; i < array.length; i++) {
-            if (array[i] === array[i+1]-1) {
-                sum++;
-            }else {
-                if (sum > sequence) {
-                    sequence = sum;
-                    sum = 0;
-                }
-            }
-        }        
     }
-
-    return sequence;  
+ 
+    var set = [];
+    var max = 1;
+ 
+    for (var i=0; i<array.length; i++)
+        set.push(array[i]);
+ 
+    for (var i=0; i<array.length; i++) {
+        var left = array[i] - 1;
+        var right = array[i] + 1;
+        var count = 1;
+ 
+        while (set.indexOf(left) != -1) {
+            count++;
+            set.splice(set.indexOf(left), 1);
+            left--;
+        }
+ 
+        while (set.indexOf(right) != -1) {
+            count++;
+            set.splice(set.indexOf(right), 1);
+            right++;
+        }
+ 
+        max = Math.max(count, max);
+    }
+ 
+    return max;
 }
